79695c0604fed1008e7e18b66b26dc0a0eeb941d
giorgioGTelian <giorgio.programmer@gmail.com>
2023-03-10 12:38:49 +0100

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./style.css";
 import Content from "./pages/content.js";
 import MovieForm from "./pages/movieform.js";
+import Fullscreen from "./core/fullscreenmode.js";
 
 export default function App() {
   
   return (
     <div>
       <MovieForm />
+      <Fullscreen />
       <Content />
     </div>
   );
