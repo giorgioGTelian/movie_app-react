ec603546b8e51297feb2b3d689148153094afc4a
giorgioGTelian <giorgio.programmer@gmail.com>
2023-03-10 12:36:01 +0100

    1 file modified, 1 deleted

--- src/pages/content.js
+++ src/pages/content.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 
+
 const APIURL = "https://api.themoviedb.org/3/discover/movie?api_key=04c35731a5ee918f014970082a0088b1";
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI =
